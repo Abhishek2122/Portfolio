98fd562856a52c56a1976aa5eb21cbd32697a388
Abhishek Singh <50039604+Abhishek2122@users.noreply.github.com>
2022-12-10 11:05:50 +0530

    Interview added

---
+++ InterView/api/assets/js/scripts_internet.js
@@ -0,0 +1,6 @@
+//JUST AN EXAMPLE, PLEASE USE YOUR OWN PICTURE!
+function getFileValue(fileName){
+    var fileContent = readTextFile(fileName);
+    return fileContent;
+}
+// export {getFileValue}
